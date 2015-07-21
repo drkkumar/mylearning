ce13e279f1dd7933dbb70eb4e56dd7fddcafc811
nithin <nithin@krishna>
2015-07-20 20:49:12 -0500

    adding new comment

--- FirstProject/src/day1/Exercise/ExerciseC.java
+++ FirstProject/src/day1/Exercise/ExerciseC.java
@@ -4,7 +4,7 @@ public class ExerciseC {
 
 	public static void main(String[] args) {
 		String  a = "Hello World";
-		System.out.println(a.substring(6).toLowerCase());
+		System.out.println(a.substring(6).toLowerCase()+"adding a new line");
 
 	}
 

6d0dc9ba1ad3b3e033198e210ea3ffb76effdfd9
nithin <nithin@krishna>
2015-07-20 21:05:03 -0500

    thrrs

--- FirstProject/src/day1/Exercise/ExerciseC.java
+++ FirstProject/src/day1/Exercise/ExerciseC.java
@@ -5,7 +5,7 @@ public class ExerciseC {
 	public static void main(String[] args) {
 		String  a = "Hello World";
 		System.out.println(a.substring(6).toLowerCase()+"adding a new line");
-		System.out.println("This is my test");
+		System.out.println("This is my test-> thrid");
 
 	}
 
